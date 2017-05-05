a76f39d889d195417ce6fd50578f335ed1814929
DESKTOP-ME3MOAG\Mukul <mukul.bajpai0821@gmail.com>
2017-05-05 09:36:32 -0700

    integrated maps with heatmap, modified template, changes in past analytics

--- routes/analytics.js
+++ routes/analytics.js
@@ -6,14 +6,14 @@ var mongo = require("./mongo");
 var mongoURL = "mongodb://cmpe280:cmpe280@ds129281.mlab.com:29281/cmpe280";
 var ejs = require('ejs');
 
-exports.getdaily=function(req,res){
+exports.getsale=function(req,res){
 
-    // var store=req.param('storename');
+     var store=req.param('storename');
 
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('analyticsdaily');
-        coll.find({"store":"walmart"}).toArray(function(err, user){
+        coll.find({"store":store}).toArray(function(err, user){
             if(user)
             {
 
@@ -31,35 +31,48 @@ exports.getdaily=function(req,res){
 
 
 };
-exports.postdaily=function(req,res){
+exports.getperson=function(req,res){
 
     var store=req.param('storename');
-    var current=req.param('current');
-    var coord=req.param('coord');
+    var start = new Date(req.param('dated'));
+    var end = new Date(req.param('dated'));
+
+
+    start.setHours(0);
+    start.setMinutes(0);
+    end.setHours(23);
+    end.setMinutes(59);
+
+
+
 
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('heatmapdaily');
-        coll.insertOne({"timestamp" : new Date(),
-            "store" : store,
-            "coord":coord
-
-        }, function(err,data){
-            if(err)
+        coll.aggregate([ {$match:{"timestamp":{$gte: start, $lt: end},"store":store}},
             {
-                console.log(err);
-                res.status(401).send({"status":"Insertion Failed"});
+                $project: {
+                    _id:0,
+                    timestamp:1,
+                    numberofpersons: { $size: "$coord" }
+                }
+            }
+        ]).toArray(function(err, result){
+            if(result)
+            {
+                console.log(result);
+                res.status(200).send({"result":result});
             }
             else
             {
-                console.log(data);
-                res.status(200).send({"status":"Successfully Inserted"});
+                res.status(401).send({"result":"Failed"});
 
             }
 
         });
 
     });
+
 
 
 };
