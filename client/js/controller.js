c3f449c3f2b3f200c48f231b5027acc78de18b51
DESKTOP-ME3MOAG\Mukul <mukul.bajpai0821@gmail.com>
2017-05-10 00:12:24 -0700

    HeatMap integrated for current page, 360 degree view added, navigation changes

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,88 +1,87 @@
-var ang= angular.module("prod",[]);
-ang.controller("ProdController", function($scope,$http){
+	var ang= angular.module("prod",[]);
+	ang.controller("ProdController", function($scope,$http){
 
-      $scope.IsVisible = false;
-            $scope.ShowHide = function (store) {
-                //If DIV is visible it will be hidden and vice versa.
-			$scope.backimage ={backgroundImage: "url('images/"+ store+"')" };
-                $scope.IsVisible = true;
-			//	alert("store"+store);
-            }
-    $scope.showModal = false;
-    $scope.toggleModal = function(img,name){
-		//alert("",img);
-        $scope.showModal = !$scope.showModal;
-    };
-$scope.targetStoreMap=false;
-$scope.walmartStoreMap=false;
-$scope.costcoStoreMap=false;
+		  $scope.IsVisible = false;
+				$scope.ShowHide = function (store) {
+					//If DIV is visible it will be hidden and vice versa.
+				$scope.backimage ={backgroundImage: "url('images/"+ store+"')" };
+					$scope.IsVisible = true;
+				
+				}
+		$scope.showModal = false;
+		$scope.toggleModal = function(img,name){
+			//alert("",img);
+			$scope.showModal = !$scope.showModal;
+		};
+	$scope.targetStoreMap=false;
+	$scope.walmartStoreMap=false;
+	$scope.costcoStoreMap=false;
 
-$scope.targetStore=function(){
-  $scope.targetStoreMap=true;
-  $scope.walmartStoreMap=false;
-  $scope.costcoStoreMap=false;
-  
+	$scope.targetStore=function(){
+	  $scope.targetStoreMap=true;
+	  $scope.walmartStoreMap=false;
+	  $scope.costcoStoreMap=false;
 
-}
-$scope.walmartStore=function(){
-  $scope.targetStoreMap=false;
-  $scope.walmartStoreMap=true;
-  $scope.costcoStoreMap=false;
+	}
+	$scope.walmartStore=function(){
+	  $scope.targetStoreMap=false;
+	  $scope.walmartStoreMap=true;
+	  $scope.costcoStoreMap=false;
 
-};
-$scope.costcoStore=function(){
-  $scope.targetStoreMap=false;
-  $scope.walmartStoreMap=false;
-  $scope.costcoStoreMap=true;
+	};
+	$scope.costcoStore=function(){
+	  $scope.targetStoreMap=false;
+	  $scope.walmartStoreMap=false;
+	  $scope.costcoStoreMap=true;
 
-}
-$scope.products =  [
-{category:'Garments',name:'Jeans',quantity:5},
-{category:'Garments',name:'Shirts',quantity:10},
-{category:'Grocery',name:'Rice',quantity:"100 Kg"},
-{category:'Grocery',name:'Bread',quantity:"100 packets"}
-];
-});
+	}
+	$scope.products =  [
+	{category:'Garments',name:'Jeans',quantity:5},
+	{category:'Garments',name:'Shirts',quantity:10},
+	{category:'Grocery',name:'Milk',quantity:"1 gallon"},
+	{category:'Grocery',name:'Bread',quantity:"100 packets"}
+	];
+	});
 
-ang.directive('modal', function () {
-    return {
-      template: '<div class="modal fade">' + 
-          '<div class="modal-dialog">' + 
-            '<div class="modal-content">' + 
-              '<div class="modal-header">' + 
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-                '<h4 class="modal-title">{{ title }}</h4>' + 
-              '</div>' + 
-              '<div class="modal-body" ng-transclude></div>' + 
-			  
-            '</div>' + 
-          '</div>' + 
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-        scope.title = attrs.title;
+	ang.directive('modal', function () {
+		return {
+		  template: '<div class="modal fade">' + 
+			  '<div class="modal-dialog">' + 
+				'<div class="modal-content">' + 
+				  '<div class="modal-header">' + 
+					'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+					'<h4 class="modal-title">{{ title }}</h4>' + 
+				  '</div>' + 
+				  '<div class="modal-body" ng-transclude></div>' + 
+				  
+				'</div>' + 
+			  '</div>' + 
+			'</div>',
+		  restrict: 'E',
+		  transclude: true,
+		  replace:true,
+		  scope:true,
+		  link: function postLink(scope, element, attrs) {
+			scope.title = attrs.title;
 
-        scope.$watch(attrs.visible, function(value){
-          if(value == true)
-            $(element).modal('show');
-          else
-            $(element).modal('hide');
-        });
+			scope.$watch(attrs.visible, function(value){
+			  if(value == true)
+				$(element).modal('show');
+			  else
+				$(element).modal('hide');
+			});
 
-        $(element).on('shown.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = true;
-          });
-        });
+			$(element).on('shown.bs.modal', function(){
+			  scope.$apply(function(){
+				scope.$parent[attrs.visible] = true;
+			  });
+			});
 
-        $(element).on('hidden.bs.modal', function(){
-          scope.$apply(function(){
-            scope.$parent[attrs.visible] = false;
-          });
-        });
-      }
-    };
-  });
+			$(element).on('hidden.bs.modal', function(){
+			  scope.$apply(function(){
+				scope.$parent[attrs.visible] = false;
+			  });
+			});
+		  }
+		};
+	  });
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/',function(req, res, next){
   res.render('index.html');
 });
-
+//changed
 router.get('/past',function(req, res, next){
   res.render('past_analytics.html');
 });
