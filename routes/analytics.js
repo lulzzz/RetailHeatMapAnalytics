632fccd0cda4f90424ffd561e5f8cca91eb887a0
Jagmohan Singh <jagmohan.singh@sjsu.edu>
2017-05-11 10:18:57 -0700

    adding analytincs and heatmap changes

--- routes/analytics.js
+++ routes/analytics.js
@@ -5,77 +5,147 @@
 var mongo = require("./mongo");
 var mongoURL = "mongodb://cmpe280:cmpe280@ds129281.mlab.com:29281/cmpe280";
 var ejs = require('ejs');
+var walmartProduct={jeans:10,shoes:12,bags:9,polos:13};
+var targetProduct={watches:10,shoes:12,sweaters:9,polos:13};
+var costcoProduct={shirts:10,watches:12,bags:9,polos:13};
 
-exports.getsale=function(req,res){
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+exports.productCatalog=function(req,res){
+    var store=req.param('store');
 
-     var store=req.param('storename');
+    if(store=="walmart"){
+        walmartProduct.jeans=walmartProduct.jeans+getRandomInt(1,3);
+        walmartProduct.shoes=walmartProduct.shoes+getRandomInt(1,3);
+        walmartProduct.bags= walmartProduct.bags+getRandomInt(1,3);
+        walmartProduct.polos=walmartProduct.polos+getRandomInt(1,3);
+        var products=[
+            {"name":"Jeans","quantity":walmartProduct.jeans},
+            {"name":"Shoes","quantity":walmartProduct.shoes},
+            {"name":"Bags","quantity":walmartProduct.bags},
+            {"name":"Polo","quantity":walmartProduct.polos},
+        ];
+    }
+    else if(store=="costco"){
+        costcoProduct.shirts=costcoProduct.shirts+getRandomInt(1,3);
+        costcoProduct.watches=costcoProduct.watches+getRandomInt(1,3);
+        costcoProduct.bags= costcoProduct.bags+getRandomInt(1,3);
+        costcoProduct.polos=costcoProduct.polos+getRandomInt(1,3);
+        var products=[
+            {"name":"Shirts","quantity":costcoProduct.shirts},
+            {"name":"Watches","quantity":costcoProduct.watches},
+            {"name":"Bags","quantity":costcoProduct.bags},
+            {"name":"Polo","quantity":costcoProduct.polos},
+        ];
 
-    mongo.connect(mongoURL, function(){
-        console.log('Connected to mongo at: ' + mongoURL);
-        var coll = mongo.collection('analyticsdaily');
-        coll.find({"store":store}).toArray(function(err, user){
-            if(user)
-            {
+    }
+    else{
+        targetProduct.watches=targetProduct.watches+getRandomInt(1,3);
+        targetProduct.shoes=targetProduct.shoes+getRandomInt(1,3);
+        targetProduct.sweaters= targetProduct.sweaters+getRandomInt(1,3);
+        targetProduct.polos=targetProduct.polos+getRandomInt(1,3);
+        var products=[
+            {"name":"Watches","quantity":targetProduct.watches},
+            {"name":"Shoes","quantity":targetProduct.shoes},
+            {"name":"Sweaters","quantity":targetProduct.sweaters},
+            {"name":"Polo","quantity":targetProduct.polos},
+        ];
 
-                res.status(200).send({"result":user});
-            }
-            else
-            {
-                res.status(401).send({"status":"Failed"});
+    }
 
-            }
 
-        });
 
-    });
-
+res.status(200).send(products);
 
 };
-exports.getProduct=function(req,res){
+exports.getanalyticsdaily=function(req,res){
 
-    var store=req.param('storename');
-    var start = new Date(req.param('dated'));
-    var end = new Date(req.param('dated'));
-
-
-    start.setHours(0);
-    start.setMinutes(0);
-    end.setHours(23);
-    end.setMinutes(59);
-    var result=[["bags",12],["shirts",12],["jeans",12],["watches",16]];
-    res.status(200).send(result);
-
+     var store=req.param('storename');
+     console.log(store);
+     var d = new Date(req.param('dated'));
+     console.log(d);
+     res.status(200).send({"result":"Success"})
 
     // mongo.connect(mongoURL, function(){
     //     console.log('Connected to mongo at: ' + mongoURL);
     //     var coll = mongo.collection('analyticsdaily');
-    //     coll.aggregate([ {$match:{"timestamp":{$gte: start, $lt: end},"store":store}},
+    //     coll.find({"store":store}).toArray(function(err, user){
+    //         if(user)
     //         {
-    //             $project: {
-    //                 _id:0,
-    //                 timestamp:1,
-    //                 bags: { $sum: "$sale" },
-    //                 shirts: { $sum: "$shirts" },
-    //                 jeans: { $sum: "$jeans" }
     //
-    //             }
-    //         }
-    //     ]).toArray(function(err, result){
-    //         if(result)
-    //         {
-    //             console.log(result);
-    //             res.status(200).send({"result":result});
+    //             res.status(200).send({"result":user});
     //         }
     //         else
     //         {
-    //             console.log(err);
-    //             res.status(401).send({"result":"Failed"});
+    //             res.status(401).send({"status":"Failed"});
     //
     //         }
     //
     //     });
     //
     // });
+
+
+};
+exports.getProduct=function(req,res){
+
+    var store=req.param('storename').toLowerCase();
+    var start = new Date(req.param('dated'));
+    var end = new Date(req.param('dated'));
+    start.setHours(0);
+    start.setMinutes(0);
+    end.setHours(23);
+    end.setMinutes(59);
+
+
+
+    mongo.connect(mongoURL, function(){
+        console.log('Connected to mongo at: ' + mongoURL);
+        if(store=="target")
+        var coll = mongo.collection('analyticstarget');
+        else if(store=="costco")
+            var coll = mongo.collection('analyticscostco');
+        else
+            var coll = mongo.collection('analyticswalmart');
+        coll.aggregate([ {$match:{"timestamp":{$gte: new Date(start), $lte: new Date(end)}}},{$unwind: "$product"},
+            {$group: {          "_id":null,
+                "jeans": {$sum: "$product.jeans" },
+                "shirts": {$sum: "$product.shirts" },
+                "polos": {$sum: "$product.polos" },
+                "watches": {$sum: "$product.watches" },
+                "bags": {$sum: "$product.bags" },
+                "shoes": {$sum: "$product.shoes" },
+                "sweaters": {$sum: "$product.sweaters" },     }}
+        ]).toArray(function(err, result){
+            if(result)
+            {
+                var output=[
+                    {"name":"jeans","y":result[0].jeans},
+                    {"name":"bags","y":result[0].bags},
+                    {"name":"watches","y":result[0].watches},
+                    {"name":"shirts","y":result[0].shirts},
+                    {"name":"polos","y":result[0].polos},
+                    {"name":"sweaters","y":result[0].sweaters},
+                    {"name":"shoes","y":result[0].shoes},
+
+                ];
+
+                console.log(output);
+                res.status(200).send(output);
+            }
+            else
+            {
+                console.log(err);
+                res.status(401).send("Failed");
+
+            }
+
+        });
+
+    });
 
 
 
--- routes/heatmap.js
+++ routes/heatmap.js
@@ -5,73 +5,73 @@ var mongo = require("./mongo");
 var mongoURL = "mongodb://cmpe280:cmpe280@ds129281.mlab.com:29281/cmpe280";
 var ejs = require('ejs');
 
-exports.getmonthly=function(req,res){
-
-    var store=req.param('storename');
-    var end = new Date(2017, 04, 30);
-    var start = new Date(2017, 04, 01);
-
-    mongo.connect(mongoURL, function(){
-        console.log('Connected to mongo at: ' + mongoURL);
-        var coll = mongo.collection('heatmapdaily');
-        coll.find({"timestamp":{$gte: start, $lt: end},"store":store}).toArray(function(err, user){
-            if(user)
-            {
-
-                res.status(200).send({"result":user});
-            }
-            else
-            {
-                res.status(401).send({"result":"Failed"});
-
-            }
-
-        });
-
-    });
-
-
-};
-exports.getyearly=function(req,res){
-
-    var store=req.param('storename');
-    var year = req.param('year');
-    var end = new Date(year, 13, 31);
-    var start = new Date(2017, 01, 01);
-
-    mongo.connect(mongoURL, function(){
-        console.log('Connected to mongo at: ' + mongoURL);
-        var coll = mongo.collection('heatmapdaily');
-        coll.find({"timestamp":{$gte: start, $lt: end},"store":store}).toArray(function(err, user){
-            if(user)
-            {
-
-                res.status(200).send({"result":user});
-            }
-            else
-            {
-                res.status(401).send({"result":"Failed"});
-
-            }
-
-        });
-
-    });
-
-
-};
+// exports.getmonthly=function(req,res){
+//
+//     var store=req.param('storename');
+//     var end = new Date(2017, 04, 30);
+//     var start = new Date(2017, 04, 01);
+//
+//     mongo.connect(mongoURL, function(){
+//         console.log('Connected to mongo at: ' + mongoURL);
+//         var coll = mongo.collection('heatmapdaily');
+//         coll.find({"timestamp":{$gte: start, $lt: end},"store":store}).toArray(function(err, user){
+//             if(user)
+//             {
+//
+//                 res.status(200).send({"result":user});
+//             }
+//             else
+//             {
+//                 res.status(401).send({"result":"Failed"});
+//
+//             }
+//
+//         });
+//
+//     });
+//
+//
+// };
+// exports.getyearly=function(req,res){
+//
+//     var store=req.param('storename');
+//     var year = req.param('year');
+//     var end = new Date(year, 13, 31);
+//     var start = new Date(2017, 01, 01);
+//
+//     mongo.connect(mongoURL, function(){
+//         console.log('Connected to mongo at: ' + mongoURL);
+//         var coll = mongo.collection('heatmapdaily');
+//         coll.find({"timestamp":{$gte: start, $lt: end},"store":store}).toArray(function(err, user){
+//             if(user)
+//             {
+//
+//                 res.status(200).send({"result":user});
+//             }
+//             else
+//             {
+//                 res.status(401).send({"result":"Failed"});
+//
+//             }
+//
+//         });
+//
+//     });
+//
+//
+// };
 exports.gethourly=function(req,res){
 
     var store=req.param('storename');
-    var end = new Date(2017, 04, 30);
-    var start = new Date(2017, 04, 01);
 
+    console.log(new Date("2017-05-01T16:30:11.480Z").getHours())
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('heatmapdaily');
-        coll.find({"timestamp":{$gte: start, $lt: end},"store":store}).toArray(function(err, user){
+        coll.find({"timestamp":new Date("2017-05-01T16:30:47.344Z"),"store":store}).toArray(function(err, user){
             if(user)
             {
+
 
                 res.status(200).send({"result":user});
             }
@@ -89,18 +89,23 @@ exports.gethourly=function(req,res){
 };
 exports.getdaily=function(req,res){
 
-    var store=req.param('storename');
-    var end = new Date(2017, 04, 30);
-    var start = new Date(2017, 04, 01);
+    var store=req.param('storename').toLowerCase();
+    var start = new Date(req.param('dated'));
+    var end = new Date(req.param('dated'));
+    start.setHours(0);
+    start.setMinutes(0);
+    end.setHours(23);
+    end.setMinutes(59);
+
 
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('heatmapdaily');
-        coll.find({"timestamp":{$gte: start, $lt: end},"store":store}).toArray(function(err, user){
-            if(user)
+        coll.find({"timestamp": {"$gte": new Date(2017,4, 1), "$lt": new Date(2017,4,2)},"store":store}).toArray(function(err, result){
+            if(result)
             {
 
-                res.status(200).send({"result":user});
+                res.status(200).send(result);
             }
             else
             {
@@ -124,6 +129,183 @@ exports.postdaily=function(req,res){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('heatmapdaily');
         coll.insertOne({"timestamp" : new Date('2016-12-04T20:19:31.157Z'),
+            "store" : store,
+            "coord":coord
+
+        }, function(err,data){
+            if(err)
+            {
+                console.log(err);
+                res.status(401).send({"status":"Insertion Failed"});
+            }
+            else
+            {
+                console.log(data);
+                res.status(200).send({"status":"Successfully Inserted"});
+
+            }
+
+        });
+
+    });
+
+
+};
+exports.postrandom=function(req,res){
+
+    var store="costco";
+    var coord=[
+        {
+            "x": 408.1,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 566.3,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 195.2,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 304.2,
+            "y": 241.3,
+            "value": 1
+        },
+        {
+            "x": 275.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 406.3,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 179.3,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 157.5,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 287.2,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 454.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 408.1,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 566.3,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 195.2,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 304.2,
+            "y": 241.3,
+            "value": 1
+        },
+        {
+            "x": 275.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 406.3,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 179.3,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 157.5,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 287.2,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 454.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 409.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 177.5,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 179.3,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 157.5,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 287.2,
+            "y": 40,
+            "value": 1
+        },
+        {
+            "x": 454.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 409.2,
+            "y": 160,
+            "value": 1
+        },
+        {
+            "x": 177.5,
+            "y": 40,
+            "value": 1
+        }
+    ];
+
+    var d = new Date();
+    d.setDate(1);
+    d.setHours(09);
+    d.setMinutes(30);
+
+    mongo.connect(mongoURL, function(){
+        console.log('Connected to mongo at: ' + mongoURL);
+        var coll = mongo.collection('heatmapdaily');
+        coll.insertOne({"timestamp" : new Date(d),
             "store" : store,
             "coord":coord
 
